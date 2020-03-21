63c7502f4f79a15fdb94a4582f730da717cb5fc4
Tahar <tntmeijs@gmail.com>
2020-03-21 15:54:16 +0100

    Empty classes for a renderer and a window

---
+++ src/core/window.hpp
@@ -0,0 +1,11 @@
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+
+namespace vi
+{
+	class Window
+	{
+	};
+}
+
+#endif //! WINDOW_HPP
---
+++ src/graphics/renderer.hpp
@@ -0,0 +1,11 @@
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
+
+namespace vi
+{
+	class Renderer
+	{
+	};
+}
+
+#endif //! RENDERER_HPP
