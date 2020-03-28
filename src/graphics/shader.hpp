5d83ba324a8b599d559792e95cd43ec01f2beb84
Tahar <tntmeijs@gmail.com>
2020-03-28 20:38:24 +0100

    Shader base class

--- src/graphics/shader.hpp
+++ src/graphics/shader.hpp
@@ -1,13 +1,33 @@
 #ifndef GRAPHICS_SHADER_HPP
 #define GRAPHICS_SHADER_HPP
 
+#include <memory>
+
 namespace vi
 {
-	class Renderer
+	/**
+	 * Base class for loading and using shaders
+	 */
+	class Shader
 	{
 	public:
+		/**
+		 * Load a new shader from a source file
+		 *
+		 * @return	True when the shader was loaded successfully, false otherwise
+		 */
+		virtual bool Load() = 0;
 
-	private:
+		/**
+		 * Get a pointer to the underlaying shader handle
+		 *
+		 * @return	Weak pointer to the shader handle
+		 */
+		virtual const std::weak_ptr<void> const Get() = 0;
+
+	protected:
+		/** Handle to use the shader in the renderer */
+		std::shared_ptr<void> m_handle;
 	};
 }
 
